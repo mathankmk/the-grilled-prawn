cec8000010d45764d96670e9168c532d867697e1
kmkmathan <kmkmathan@gmail.com>
2019-12-02 18:46:17 +0530

    responsive and validation has done

--- assets/js/default.js
+++ assets/js/default.js
@@ -10,15 +10,18 @@ const returnToTop = () => {
 const formHtml = random => ` <div class="d--flex newsletter__content">
 <div class="newsletter__item ">
     <label class="newsletter__label" for="name__${random}">Name</label>
-    <input class="newsletter__input newsletter__name" id="name__${random}" type="text" />
+    <input class="newsletter__input newsletter__name" id="name__${random}" type="text" maxlength="50"/>
+    <p class="name__${random} newsletter__error"></p>
 </div>
 <div class="newsletter__item">
     <label class="newsletter__label" for="surname__${random}">Surname</label>
-    <input class="newsletter__input newsletter__surname" type="text" id="surname__${random}"/>
+    <input class="newsletter__input newsletter__surname" type="text" id="surname__${random}" maxlength="50"/>
+    <p class="surname__${random} newsletter__error"></p>
 </div>
 <div class="newsletter__item">
     <label class="newsletter__label" for="email__${random}">Email address</label>
-    <input class="newsletter__input newsletter__email" type="text" id="email__${random}"/>
+    <input class="newsletter__input newsletter__email" type="text" id="email__${random}"  maxlength="50"/>
+    <p class="email__${random} newsletter__error"></p>
 </div>
 <div class="newsletter__item d--flex newsletter__removecontent hide">
     <div onClick="removeFriend(${random})" class="d--flex"><img class="newsletter__remove" alt="The Grilled Prawn - remove image" src="assets/images/delete.png" />
@@ -75,7 +78,14 @@ const sendFormData = () => {
   let surnameArray = document.querySelectorAll(".newsletter__surname");
   let emailArray = document.querySelectorAll(".newsletter__email");
   let dataInstance = new FormFieldData(nameArray, surnameArray, emailArray);
-  console.log(dataInstance.getData());
+
+  if (dataInstance.getData().length > 0) {
+    console.log(dataInstance.getData());
+    alert("Invitations sent successfully");
+    for (let i = 0; i < nameArray.length; i++) {
+      emailArray[i].value = surnameArray[i].value = nameArray[i].value = "";
+    }
+  }
 };
 
 // Class for format the form field data
@@ -87,13 +97,66 @@ class FormFieldData {
   }
   getData() {
     let jsonData = [];
+    let isError = false;
+    let emailErrorId = "";
     for (let i = 0; i < this.names.length; i++) {
+      this.names[i].value = this.names[i].value.trim();
+      this.surnames[i].value = this.surnames[i].value.trim();
+      this.emails[i].value = this.emails[i].value.trim();
+      document.querySelector(`.${this.names[i].id}`).innerHTML = "";
+      document.querySelector(`.${this.surnames[i].id}`).innerHTML = "";
+      document.querySelector(`.${this.emails[i].id}`).innerHTML = "";
+
+      for (let object of jsonData) {
+        if (object.email == this.emails[i].value) {
+          emailErrorId = this.emails[i].id;
+          break;
+        }
+      }
+      if (
+        this.names[i].value == "" ||
+        !/^[A-Za-z ]+$/.test(this.names[i].value)
+      ) {
+        document.querySelector(
+          `.${this.names[i].id}`
+        ).innerHTML = `Please enter valid name`;
+        isError = true;
+        break;
+      } else if (
+        this.surnames[i].value == "" ||
+        !/^[A-Za-z ]+$/.test(this.surnames[i].value)
+      ) {
+        document.querySelector(
+          `.${this.surnames[i].id}`
+        ).innerHTML = `Please enter valid surname`;
+        isError = true;
+        break;
+      } else if (
+        this.emails[i].value == "" ||
+        !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+          this.emails[i].value
+        )
+      ) {
+        document.querySelector(
+          `.${this.emails[i].id}`
+        ).innerHTML = `Please enter valid email`;
+        isError = true;
+        break;
+      } else if (emailErrorId.length > 0) {
+        document.querySelector(
+          `.${this.emails[i].id}`
+        ).innerHTML = `Email Id already entered`;
+        isError = true;
+        break;
+      }
+
       jsonData.push({
         name: this.names[i].value,
         surname: this.surnames[i].value,
         email: this.emails[i].value
       });
     }
+    if (isError) return [];
     return jsonData;
   }
 }
